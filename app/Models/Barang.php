47a6f427e6359b296fa6a40484aed5058e41d591
feny23 <ffenymametri@gmail.com>
2020-11-14 18:46:06 -0800

    migrate, factory, model

---
+++ app/Models/Barang.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    use HasFactory;
+
+    protected $table = "barang";
+
+    public function kategori(){
+        return $this->belongsTo('App\Models\Kategori');
+    }
+    public function transaksi_details(){
+        return $this->hasMany('App\Models\TransaksiDetail');
+    }
+}
